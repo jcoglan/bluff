c43933cb734727b0a5154f30032720f3cc1d11c1
James Coglan <jcoglan@googlemail.com>
2008-09-08 14:05:43 +0100

    Fixing gravity-choosing bug in Net graph.

--- source/net.js
+++ source/net.js
@@ -111,13 +111,13 @@ Bluff.Net = new JS.Class(Bluff.Base, {
     var s = rad_pos / (2*Math.PI);
     switch (true) {
       case s >= 0.96 || s < 0.04  :   this._d.gravity = 'south';      break;
-      case s >= 0.04 || s < 0.21  :   this._d.gravity = 'west';       break;  // southwest
-      case s >= 0.21 || s < 0.29  :   this._d.gravity = 'west';       break;
-      case s >= 0.29 || s < 0.46  :   this._d.gravity = 'west';       break;  // northwest
-      case s >= 0.46 || s < 0.54  :   this._d.gravity = 'north';      break;
-      case s >= 0.54 || s < 0.71  :   this._d.gravity = 'east';       break;  // northeast
-      case s >= 0.71 || s < 0.79  :   this._d.gravity = 'east';       break;
-      case s >= 0.79 || s < 0.96  :   this._d.gravity = 'east';       break;  // southest
+      case s >= 0.04 && s < 0.21  :   this._d.gravity = 'west';       break;  // southwest
+      case s >= 0.21 && s < 0.29  :   this._d.gravity = 'west';       break;
+      case s >= 0.29 && s < 0.46  :   this._d.gravity = 'west';       break;  // northwest
+      case s >= 0.46 && s < 0.54  :   this._d.gravity = 'north';      break;
+      case s >= 0.54 && s < 0.71  :   this._d.gravity = 'east';       break;  // northeast
+      case s >= 0.71 && s < 0.79  :   this._d.gravity = 'east';       break;
+      case s >= 0.79 && s < 0.96  :   this._d.gravity = 'east';       break;  // southest
     }
     this._d.annotate_scaled(0, 0, x, y, amount, this._scale);
   }
